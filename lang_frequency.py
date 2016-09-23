ba1c1a03392cccc96e2220bcd2302feb36e81973
Your Name <you@example.com>
2016-09-23 16:16:41 +0300

    реализация задачи, соблюдение PEP8

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,24 @@
+import re
+from collections import Counter
 
 
 def load_data(filepath):
-    pass
+    # чтение файла в WIN-кодировке
+    return open(filepath, 'r', encoding="cp1251")
 
 
 def get_most_frequent_words(text):
-    pass
-
+    tekst = (text.read()).lower()  # перевести весь текст в нижний регистр
+    result = re.findall(r'\w+', tekst)  # поиск слов с сохранением в список
+    for ind, slovo in enumerate(result):  # просмотр всех слов
+        if not slovo.isalpha():  # если слово содержит цифру, удаляем из списка
+            del result[ind]
+    # возвращает список 10 популярных слов
+    return Counter(result).most_common(10)
 
 if __name__ == '__main__':
-    pass
+    f = load_data('tolstoi_l_n__voina_i_mir.txt')
+    popular_words = get_most_frequent_words(f)
+    print("10 самых популярных слов в этом файле:")
+    for words in popular_words:
+        print("==> слово '", words[0], "' частота - ", words[1])
